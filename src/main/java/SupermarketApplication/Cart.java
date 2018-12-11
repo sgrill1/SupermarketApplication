9bf3c3bcce9a07312cdf6d0cecc098782420bc34
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-11 16:02:53 +0000

    create a cart class

---
+++ src/main/java/SupermarketApplication/Cart.java
@@ -0,0 +1,33 @@
+package SupermarketApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cart {
+
+    private Set<Items> cart = new HashSet<>();
+    private double cartTotalValue;
+
+
+        public void addItemsToCart(Items items){
+            cart.add(items);
+        }
+
+        public void printCartItems(){
+            for (Items items: cart){
+                System.out.println(items.getQuantity().toString() +" "+ items.getItem() + "'s");
+            }
+        }
+
+        public double getCartTotalValue(){
+            cartTotalValue = 0.00;
+            for (Items items: cart) {
+                double value = items.getMultipleItemsCost();
+                cartTotalValue = cartTotalValue + value;
+            }
+            return cartTotalValue;
+
+        }
+
+
+}
---
+++ src/main/java/SupermarketApplication/Items.java
@@ -0,0 +1,29 @@
+package SupermarketApplication;
+
+
+public class Items {
+
+    private Integer quantity;
+    private Item item;
+    private double multipleItemsCost;
+
+    public Items(Integer selectQuantity, Item item) {
+        this.quantity = selectQuantity;
+        this.item = item;
+        this.multipleItemsCost = item.getUnitCost() * quantity;
+    }
+
+
+    //Getters
+    public String getItem() {
+        return item.getItemName();
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public double getMultipleItemsCost() {
+        return multipleItemsCost;
+    }
+}
