c3fe344a2e8f17c7869c35a23b3f0522e9600c76
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-11 15:58:01 +0000

    edit item class

--- src/main/java/SupermarketApplication/Item.java
+++ src/main/java/SupermarketApplication/Item.java
@@ -1,4 +1,29 @@
 package SupermarketApplication;
 
+import java.util.HashMap;
+
 public class Item {
+    private String itemName;
+    private double unitCost;
+    private boolean availableDiscount;
+
+    public Item(String itemName, double unitCost, boolean availableDiscount) {
+        this.itemName = itemName;
+        this.unitCost = unitCost;
+        this.availableDiscount = availableDiscount;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getUnitCost() {
+        return unitCost;
+    }
+
+    public boolean getIsDiscountAvailable() {
+        return availableDiscount;
+    }
+
 }
