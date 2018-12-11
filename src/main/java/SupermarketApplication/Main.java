e7818c79409952d1e7ae8676cdf09c2eabe713e3
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-11 16:03:25 +0000

    run class

--- src/main/java/SupermarketApplication/Main.java
+++ src/main/java/SupermarketApplication/Main.java
@@ -4,6 +4,24 @@ package SupermarketApplication;
 public class Main
 {
     public static void main( String[] args ) {
+        Item apple = new Item("Apple", 0.53, false);
+        Item banana = new Item("Banana",0.98,true);
+        Item pear = new Item("Pear", 0.68,true);
+
+        Items apples = new Items(4,apple);
+        Items bananas = new Items(5,banana);
+        Items pears = new Items(3,pear);
+
+        Cart cart = new Cart();
+        cart.addItemsToCart(bananas);
+        cart.addItemsToCart(pears);
+        cart.addItemsToCart(apples);
+
+        cart.printCartItems();
+        System.out.println(cart.getCartTotalValue());
+
+
+
 
 
 
