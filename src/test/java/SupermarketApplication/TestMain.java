45dc3103ec81a706c97c5cd93a2229cdb5c6c2ae
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-17 16:44:22 +0000

    add tests for item class

--- src/test/java/SupermarketApplication/TestMain.java
+++ src/test/java/SupermarketApplication/TestMain.java
@@ -1,20 +1,46 @@
 package SupermarketApplication;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestMain {
 
-    Item apple = new Item("apple",0.50,"bogof");
-    Items apples = new Items(5, apple);
+    Item apple = null;
+    Items apples = null;
+
+    @BeforeClass
+    public void setup() {
+            try {
+                apple = new Item("apple", 0.50, "bogof"); }
+            catch (InvalidPriceException e) {
+                e.printStackTrace();
+            }
+            new Items(5, apple);
+    }
+
+    //Test Item class
+
+    @Test
+    public void checkItemUnitCostIsRoundedCorrectly() throws InvalidPriceException{
+        Item newItem = new Item("newItem",223.5372,"any");
+        Assert.assertEquals(newItem.getUnitCost(),223.54,0.001);
+    }
+
+    @Test(expected = InvalidPriceException.class)
+    public void createItemWithInvalidPrice() throws InvalidPriceException{
+        Item newItem = new Item("newItem", -0.01,"any");
+    }
 
     //Test Items class
 
     @Test
+    public void createItemsObjectWithNegativeQuantity() throws InvalidPriceException {
+        Item anyItem = new Item("the item",2.00, "null");
+        Items anyItems = new Items(-2,anyItem);
+        Assert.assertEquals(anyItems.getQuantity(),(Integer) 1);
+    }
 
     @Test
     public void checkIncreaseItemQuantity(){
@@ -45,6 +71,10 @@ public class TestMain {
         apples.decreaseQuantity(7);
         Assert.assertEquals(apples.getQuantity(),(Integer) 0);
     }
+
+    // Cart Tests
+
+
 
 
 
