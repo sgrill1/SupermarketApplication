7281176f8097a0678822918827dc7b6388dc9a2d
Sam Grill <samgrill@Sams-MacBook-Pro.local>
2018-12-17 21:06:54 +0000

    move sout checks into TestMain

--- src/main/java/SupermarketApplication/Main.java
+++ src/main/java/SupermarketApplication/Main.java
@@ -3,19 +3,7 @@ package SupermarketApplication;
 
 public class Main {
         public static void main(String[] args) {
-                Item apple = new Item("apple", 0.855, "bogof");
-                Items apples = new Items(9, apple);
 
-                Cart cart = new Cart(apples);
-
-                Item banana = new Item("banana", 0.67, "three for two");
-                Items bananas = new Items(7, banana);
-
-
-                cart.addNewItemsToCart(bananas);
-
-
-                Checkout checkout = new Checkout(cart, cart.getCartTotalValue());
 
         }
 }
